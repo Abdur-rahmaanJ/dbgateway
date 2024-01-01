4053265efccbca3e1c8120dd113d0af677d4d8f4
Abdur-RahmaanJ <cryptolabour@gmail.com>
2024-01-01 22:40:06 +0400

    feat: general-method

--- gateway.py
+++ gateway.py
@@ -2,9 +2,21 @@ from blinker import signal
 
 class Callback:
 
+    
+
+    @classmethod
+    def general(cls, type_):
+        return cls.methods[type_]
+
     @classmethod
     def on_server_connected(cls, sender):
         pass
+
+    @classmethod
+    def methods(cls):
+        return {
+        'on_server_connected': cls.on_server_connected
+    }
 
 class ConnectionPool:
     def __init__(self) -> None:
@@ -12,7 +24,7 @@ class ConnectionPool:
 
     def add(conenction_obj, type_):
         ready = signal(f'{type_}_connected')
-        ready.connect(Callback.on_server_connected)
+        ready.connect(Callback.methods['on_server_connected'])
 
     def remove(connection_obj):
         pass 
